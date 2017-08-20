acffd6468d8d8fae4c09d79bd55f0c84f26aeaaf
hmandal <hmandal007@gmail.com>
2017-08-20 17:45:34 +0530

    step-02: Create Todo View.

--- scripts/App.js
+++ scripts/App.js
@@ -16,4 +16,31 @@ $(function() {
     }
 
   });
+
+  // Todo Item View
+  // --------------
+
+  // The DOM element for a todo item...
+  var TodoView = Backbone.View.extend({
+
+    //... is a list tag.
+    tagName: "li",
+
+    // Cache the template function for a single item.
+    template: _.template($('#item-template').html()),
+
+    // The TodoView listens for changes to its model, re-rendering. Since there's
+    // a one-to-one correspondence between a **Todo** and a **TodoView** in this
+    // app, we set a direct reference on the model for convenience.
+    initialize: function() {
+      this.listenTo(this.model, 'change', this.render);
+    },
+
+    // Re-render the titles of the todo item.
+    render: function() {
+      this.$el.html(this.template(this.model.toJSON()));
+      return this;
+    }
+
+  });
 });
